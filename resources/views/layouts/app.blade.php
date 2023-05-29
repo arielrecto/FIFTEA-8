84cfcb512f4ef6c314ae8211f5700f695ff260c6
arielrecto <arielrecto@gmail.com>
2023-05-30 05:17:10 +0800

    fixed product bugs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,7 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
@@ -24,7 +25,8 @@
                 {{ $slot }}
             </main>
 
-            
+
         </div>
     </body>
+
 </html>
