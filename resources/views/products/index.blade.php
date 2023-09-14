b53378136eca0e6b28e953f7904fc27a233d865a
joshua salceda <joshuasalceda0021@gmail.com>
2023-09-14 11:48:38 +0800

    commiting minor changes on product.index

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -195,8 +195,8 @@
 
     <script>
         function sample() {
-            // const baseUrl = "http://localhost:8000";
-            const baseUrl = "http://127.0.0.1:8000";
+            const baseUrl = "http://localhost:8000";
+            // const baseUrl = "http://127.0.0.1:8000";
             return {
 
                 products: [],
