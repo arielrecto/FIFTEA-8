fcb79211e9c0b455658405b21aa5514a5a31ff4d
Joshua <joshuasalceda0021@gmail.com>
2024-01-02 16:14:26 +0800

    UI changes: Fixing responsiveness

--- resources/views/users/client/dashboard.blade.php
+++ resources/views/users/client/dashboard.blade.php
@@ -160,7 +160,8 @@
                                     bg-gray-200 bg-opacity-50 rounded cursor-pointer hover:bg-gray-700 hover:text-white'></i>
                                 </a>
                                 <div class="w-full h-32 rounded-t bg-gradient-to-r from-green-200 to-blue-200"></div>
-                                <img src="{{ asset('storage/profile/' . $profile->image) }}" alt=""
+                                <img src="{{ $profile->image ? asset('storage/profile/' . $profile->image) : '' }}"
+                                    alt=""
                                     class="w-36 h-36 rounded-full absolute border border-gray-200 top-12 left-1/2 transform -translate-x-1/2">
                             </div>
                             <div class="pt-20 flex flex-col items-center justify-start">
