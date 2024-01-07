8ba791c77e7592f44b5794b4367c63e216316d5d
arielrecto <arielrecto29@gmail.com>
2024-01-08 02:10:09 +0800

    update:test

--- app/Actions/Admin/Product/StoreProductAction.php
+++ app/Actions/Admin/Product/StoreProductAction.php
@@ -21,7 +21,7 @@ class StoreProductAction
 
         $filename = 'product' . uniqid() . '.' . $request->image->extension();
 
-        $request->image->storeAs('public/product/image/' . $filename);
+
 
         $product = Product::create([
             'image' =>  asset('/storage/product/image/'  . $filename),
@@ -35,7 +35,10 @@ class StoreProductAction
 
         $product->categories()->attach($category->id);
 
+        $request->image->storeAs('public/product/image/' . $filename);
 
+
+        return $product;
 
     }
 }
